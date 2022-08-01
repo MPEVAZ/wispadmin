a65b95b4af7a099dec20e0dd0aaccabc749f077c
mapevaz2909 <alex.pevaz@gmail.com>
2022-07-31 22:39:00 -0500

    agregamos display_package y display_price en display list de paymentAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,4 @@
+from http import client
 from django.contrib import admin
 
 from core.models import Zon,Pack,Client,Payment
@@ -19,8 +20,16 @@ class ClientAdmin(admin.ModelAdmin):
 
 class PaymentAdmin(admin.ModelAdmin):
     readonly_fields = ('created','update')
+    list_display = ('client','name','display_package','display_price','paid_out')
+    list_filter = ('paid_out','client__pay_day')
 
+    def display_package(self,obj):
+        return obj.client.package
 
+    def display_price(self,obj):
+        return obj.client.package.price
+    display_price.short_description = "Costo"
+    display_package.short_description = "Plan contratado" 
 
 
 admin.site.register(Zon,ZonAdmin)
